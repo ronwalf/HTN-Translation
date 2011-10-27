1f0efcd801aa08af142fbdc5de258868ac2695cb
Ron Alford <ronwalf@volus.net>
2011-10-26 20:23:14 -0400

    Directory cleanup

--- robot-combined/gen-suite.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for (( d=10; d<=200; d += 10 )); do
-  echo "On $d"
-  ./gen-test.sh $d $1
-done
-
--- robot-combined2/gen-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-for (( i=${1}; i<=${2}; i+=10 )); do
-  ../../genRobot2 ${i} prob${i}
-done
-
